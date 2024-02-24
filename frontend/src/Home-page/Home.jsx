6114390ed7e90ffb8cd096b8e2847895f0f41c79
Alan Gomez <agomezh1901@alumno.ipn.mx>
2024-02-24 12:26:35 -0600

    Verificando el home para que muestre su rol aun falla

--- frontend/src/Home-page/Home.jsx
+++ frontend/src/Home-page/Home.jsx
@@ -2,25 +2,41 @@ import React, { useEffect, useState } from 'react';
 import Navbar from './Navbar';
 import User from './User';
 import Admin from './Admin';
-import jwt_decode from 'jwt-decode';
+import { jwtDecode } from "jwt-decode";
 
 const Home = () => {
     const [userRoles, setUserRoles] = useState([]);
 
     useEffect(() => {
-        // Obtener el token JWT del almacenamiento local
+        // Obtener el token del almacenamiento local
         const token = localStorage.getItem('token');
-        // Decodificar el token JWT para obtener información sobre los roles del usuario
+        console.log(token); // Agrega esta línea
+    
         if (token) {
-            const decodedToken = jwt_decode(token);
-            setUserRoles(decodedToken.authorities.map(auth => auth.authority));
+          try {
+            // Decodificar el token JWT
+            const decodedToken = jwtDecode(token);
+            
+            // Verificar si el token decodificado tiene la propiedad 'authorities'
+            if (decodedToken && decodedToken.authorities) {
+              // Obtener los roles del usuario desde la carga útil (payload) del token
+              const roles = decodedToken.authorities.map(authority => authority.authority);
+      
+              // Actualizar el estado con los roles del usuario
+              setUserRoles(roles);
+            }
+          } catch (error) {
+            console.error('Error al decodificar el token', error);
+            console.log('Token:', token); // Agrega esta línea para ver el token en caso de error
         }
-    }, []);
+        
+        }
+      }, []);
 
     return (
         <div className="flex items-center justify-center h-screen">
             <Navbar />
-            {userRoles.includes('ROLE_ADMIN') ? <Admin /> : <User />}
+            {userRoles.includes('ROLE_ADMIN') && !userRoles.includes('ROLE_USER') ? <Admin /> : <User />}
         </div>
     );
 };
